ccf030c2a300c0e8dc0ced2a32fce457574d267b
sebastianpinedaar <sebastianpinedaarango@gmail.com>
2017-04-14 21:53:26 -0500

    starter_code

---
+++ starter_code.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Starter code for basic level
+Colomb-ia
+
+"""
+
+#Importar librerías
+import pandas as pd
+
+#Leer datos
+train = pd.read_csv("data/iris_train.csv")
+test = pd.read_csv("data/iris_test.csv")
+
+#Explorar primeros registros
+train.head()
+train.head()
+
